2e2976ef1ccdeb526fc1d5231c14d0c851323154
rileyrg <rileyrg@gmx.de>
2023-12-14 00:09:57 +0100

    chap 18

--- App.js
+++ App.js
@@ -3,18 +3,20 @@ import { createStackNavigator } from "react-navigation-stack";
 
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
-  {
-      Home: HomeScreen,
-      Components: ComponentsScreen,
-  },
-  {
-    initialRouteName: "Components",
-    defaultNavigationOptions: {
-      title: "App",
+    {
+        Home: HomeScreen,
+        Components: ComponentsScreen,
+        List: ListScreen,
     },
-  }
+    {
+        initialRouteName: "List",
+        defaultNavigationOptions: {
+            title: "App",
+        },
+    }
 );
 
 export default createAppContainer(navigator);
