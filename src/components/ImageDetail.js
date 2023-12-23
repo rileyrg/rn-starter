251323bb299204f7241258495bd74f62f1d42fe1
rileyrg <rileyrg@gmx.de>
2023-12-23 06:20:16 +0100

    chapter 39

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -7,6 +7,7 @@ const ImageDetail = (props) => {
         <View>
             <Image source={props.imageSource}/>
             <Text>{props.title}</Text>
+            <Text>Image score - {props.imageScore}</Text>
         </View>
     );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -3,11 +3,13 @@ import { View, Text, StyleSheet } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-    return    (<View>
-                   <ImageDetail title="Forest" imageSource={require ("../../assets/forest.jpg")}/>
-                   <ImageDetail title="Beach" imageSource={require ("../../assets/beach.jpg")}/>
-                   <ImageDetail title="Mountain" imageSource={require ("../../assets/mountain.jpg")}/>
-               </View>);
+    return    (
+        <View>
+            <ImageDetail title="Forest"   imageScore="1" imageSource={require ("../../assets/forest.jpg")}/>
+            <ImageDetail title="Beach"    imageScore="2" imageSource={require ("../../assets/beach.jpg")}/>
+            <ImageDetail title="Mountain" imageScore="3" imageSource={require ("../../assets/mountain.jpg")}/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
