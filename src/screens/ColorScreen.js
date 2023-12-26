c9362865150e0377e954a03de9508b0983f3d8e4
rileyrg <rileyrg@gmx.de>
2023-12-26 03:10:08 +0100

    chapter 45

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -7,8 +7,16 @@ const ColorScreen = () => {
         <View>
             <Button title="Add A Color" onPress = {()=> {
             }}/>
+            <View style={{height:100,width:100,backgroundColor: randomRGB()}}/>
         </View>
     );
+};
+
+const randomRGB = () => {
+    const red=Math.floor(Math.random()*256);
+    const green=Math.floor(Math.random()*256);
+    const blue=Math.floor(Math.random()*256);
+    return `rgb(${red},${green},${blue})`;
 };
 
 const styles = StyleSheet.create({
