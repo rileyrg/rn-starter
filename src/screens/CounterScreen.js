1a11701be4152c9cc19dd9255b459970dd90f5aa
rileyrg <rileyrg@gmx.de>
2023-12-26 01:59:23 +0100

    chapter 42 : useState working

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { View, Text, StyleSheet,Button } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
 
 const CounterScreen = () => {
-    //todo: fix this
-    let counter=0;
+    const [counter,setCounter] = useState(0);
     return    (
         <View>
             <Button title="Increase" onPress = {()=> {
-                counter++;
+                setCounter(counter+1);
                 console.log(counter);
             }}/>
             <Button title="Decrease" onPress = {()=> {
-                counter--;
+                setCounter(counter-1);
                 console.log(counter);
             }}/>
             <Text>Current count: {counter}</Text>
