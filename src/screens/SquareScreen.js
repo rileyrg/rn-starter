0f502d5110cb1fa66f8172be91b83f5f9112cbdd
rileyrg <rileyrg@gmx.de>
2023-12-27 23:27:51 +0100

    chapter 54 hacked

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,28 +2,39 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from "../components/ColorCounter.js";
 
-const COLOR_INCREMENT=15;
+const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
     console.log(red);
+
+    const checkColorValue = (colorValue) => {
+        if (colorValue > 255)
+            return 255;
+        else if (colorValue <= 0)
+            return 0;
+        else
+            return color;
+    }
+
+
     return (
         <View>
             <ColorCounter
-                onIncrease={() =>  setRed(red + COLOR_INCREMENT) }
-                onDecrease={() =>  setRed(red - COLOR_INCREMENT) }
+                onIncrease={() => setRed(checkColorValue(red + COLOR_INCREMENT))}
+                onDecrease={() => setRed(checkColorValue(red - COLOR_INCREMENT))}
                 color="Red" />
             <ColorCounter
-                onIncrease={() => { setGreen(green + COLOR_INCREMENT) }}
-                onDecrease={() => { setGreen(green - COLOR_INCREMENT) }}
+                onIncrease={() =>  setGreen(checkColorValue(green + COLOR_INCREMENT)) }
+                onDecrease={() =>  setGreen(checkColorValue(green - COLOR_INCREMENT)) }
                 color="Green" />
             <ColorCounter
-                onIncrease={() => { setBlue(blue + COLOR_INCREMENT) }}
-                onDecrease={() => { setBlue(blue - COLOR_INCREMENT) }}
+                onIncrease={() => setBlue(checkColorValue(blue + COLOR_INCREMENT)) }
+                onDecrease={() => setBlue(checkColorValue(blue - COLOR_INCREMENT)) }
                 color="Blue" />
-            <View style={{ height: 150, width: 150, backgroundColor:`rgb(${red},${green},${blue})` }} />
+            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }} />
         </View>
     );
 };
