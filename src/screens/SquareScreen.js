52a8c20138e43f1e2bfd75c3be7685c040bd99e1
rileyrg <rileyrg@gmx.de>
2023-12-27 20:07:05 +0100

    chapter 51

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from "../components/ColorCounter.js";
 
 const SquareScreen = () => {
-    return    (
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+    console.log(red);
+    return (
         <View>
-            <ColorCounter color="Red"/>
-            <ColorCounter color="Green"/>
-            <ColorCounter color="Blue"/>
+            <ColorCounter
+                onIncrease={() => { setRed(red + 1) }}
+                onDecrease={() => { setRed(red - 1) }}
+                color="Red" />
+            <ColorCounter
+                onIncrease={() => { setGreen(green + 1) }}
+                onDecrease={() => { setGreen(green - 1) }}
+                color="Green" />
+            <ColorCounter
+                onIncrease={() => { setBlue(blue + 1) }}
+                onDecrease={() => { setBlue(blue - 1) }}
+                color="Blue" />
         </View>
     );
 };
