e1b3b46cb1e2fb1f8cc037e0125256214c65deca
rileyrg <rileyrg@gmx.de>
2023-12-28 11:31:34 +0100

    exercise 61 horrible solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,28 @@
-import React,{useReducer} from 'react';
-import { View, Text, StyleSheet,Button } from 'react-native';
+import React, { useReducer } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const reducer = (state,action) =>{
-    console.log("in reducer",state);
-    return {...state, counter: state.counter+action.amount};
+const reducer = (state, action) => {
+    // state === {counter:nunber}
+    // action === {type:'increment'||'decrement', payload:1 }
+    console.log("in reducer", state);
+    switch (action.type) {
+        case 'increment': return { ...state, counter: state.counter + action.payload };
+        case 'decrement': return { ...state, counter: state.counter - action.payload };
+        default: return state;
+    }
 };
 
 const CounterScreen = () => {
-    const [state,dispatch] = useReducer(reducer,{counter:0});
-    const {counter} = state;
-    return    (
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+    const { counter } = state;
+    return (
         <View>
-            <Button title="Increase" onPress = {()=> {
-                dispatch({amount:1});
-            }}/>
-            <Button title="Decrease" onPress = {()=> {
-                dispatch({amount:-1});
-            }}/>
+            <Button title="Increase" onPress={() => {
+                dispatch({ type:'increment', payload:1 });
+            }} />
+            <Button title="Decrease" onPress={() => {
+                dispatch({ type:'decrement', payload:1 });
+            }} />
             <Text>Current count: {counter}</Text>
         </View>
     );
