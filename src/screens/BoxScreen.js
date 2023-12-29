c19e0fdc99a8c48f811ae349a861649c35f67b15
rileyrg <rileyrg@gmx.de>
2023-12-29 22:37:49 +0100

    chapter 70

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,16 +1,28 @@
-// https://www.udemy.com/share/101Wbw3@9dbyMreEbfCKvgiTnrpP8YsaQV44sHFGKRbw-JaYwXX_q3Q_FsL3gdjkDY64odU=/
+//https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15706932#notes
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>BoxScreen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>
+        BoxScreen
+      </Text>
     </View>
   );
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black'
+  },
+  textStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+    margin:10,
+  }
+});
 
 export default BoxScreen;
