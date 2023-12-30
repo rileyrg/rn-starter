25b87d694e49e28c8a21cb29357282fe20b2b7f7
rileyrg <rileyrg@gmx.de>
2023-12-30 06:02:31 +0100

    chapter 78

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -26,7 +26,12 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'red',
     fontSize:18,
-    left:10
+    // position:'absolute',
+    // top:0,
+    // bottom:0,
+    // right:0,
+    // left:0,
+    ...StyleSheet.absoluteFillObject
   },
   textThreeStyle: {
     borderWidth: 3,
